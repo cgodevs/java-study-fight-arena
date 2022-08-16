2944319a34787e3b6156e4b796714fc1e1d3191a
cgodevs <kharol.go@gmail.com>
2022-08-16 19:30:17 -0300

    Implement new Exception

---
+++ src/com/java/fightarena/exceptions/FullSlotsException.java
@@ -0,0 +1,29 @@
+package com.java.fightarena.exceptions;
+
+public class FullSlotsException extends RuntimeException {
+
+	public FullSlotsException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FullSlotsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FullSlotsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FullSlotsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FullSlotsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
