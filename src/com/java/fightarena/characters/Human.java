1019295106573859efd25de6642bbafd09df74e1
cgodevs <kharol.go@gmail.com>
2022-08-16 19:44:26 -0300

    Add constructor, currentWeapon getter and setter, change pickUpWeapon and toString methods

--- src/com/java/fightarena/characters/Human.java
+++ src/com/java/fightarena/characters/Human.java
@@ -1,54 +1,82 @@
 package com.java.fightarena.characters;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import com.java.fightarena.abstractions.*;
+import com.java.fightarena.exceptions.FullSlotsException;
 
-
-public class Human {
-	private Weapon currentWeapon;  // Guns or Melee Weapons	
-	private ArrayList<Weapon> weapons = new ArrayList<Weapon>(4); //4 slots mean only 4 weapons may be carried at a time
+public class Human { 
+	private String name;
+	private Weapon currentWeapon; // Either a Gun or Melee Weapon
+	private ArrayList<Weapon> weapons = new ArrayList<Weapon>(4); // Only 4 weapons may be carried at a time
 	
-	public int getNumberOfWeapons() {
-		return this.weapons.size();
+	public Human(String name){
+		this.name = name;
+	}
+	
+	public Weapon getCurrentWeapon() {
+		return this.currentWeapon;
+	}
+		 
+	public void setCurrentWeapon(Weapon weapon) { 
+		this.currentWeapon = weapon; 
 	}
 
 	// POLIMORFISM taken
-	  public void pickUpWeapon(Weapon weapon) { //TODO throw exception for full slots
-		  if (this.weapons.size() == 0) {
-			  this.switchWeapon(weapon);
-		  } else if(weapon.getType() == this.currentWeapon.getType()) {
-			  try {
-				((Gun) this.currentWeapon).reload();
-			  } catch (ClassCastException ex) { // Tried to reload Melee weapon before
-				((MeleeWeapon) this.currentWeapon).restore();  
-			  }
-		  } else {
-			  System.out.println("Picked up a " + weapon.getType());
-			  this.weapons.add(weapon);
-		  } 
-	  }	 
+	public void pickUpWeapon(Weapon weapon) throws FullSlotsException {
+		int numberOfWeapons = this.weapons.size(); 
+		ArrayList<String> weaponTypes = new ArrayList<String>();
+		for (Weapon w: this.weapons) {
+			weaponTypes.add(w.getType());
+		}
+		if (numberOfWeapons == 0) {
+			System.out.println("Picked up a " + weapon.getType());
+			this.setCurrentWeapon(weapon);
+			this.weapons.add(weapon);
+		} else if (weaponTypes.contains(weapon.getType())) {
+			try {
+				for (int i = 0; i <= this.weapons.size(); i++) {
+					if (this.weapons.get(i).getType() == weapon.getType()) {
+						Gun gun = (Gun) this.weapons.get(i);
+						gun.reload(gun.getMagazineSize());
+						break;
+					}
+				}				
+			} catch (ClassCastException ex) { // Tried to reload Melee weapon before
+				((MeleeWeapon) this.getCurrentWeapon()).restore();
+			}
+		} else if (numberOfWeapons == 4) {
+			throw new FullSlotsException("You can't pick up a new weapon. Empty 1 or more slots.");
+		} else {
+			System.out.println("Picked up a " + weapon.getType());
+			this.weapons.add(weapon);
+		}
+	}
 
-	public void switchWeapon(Weapon weapon) {
-		this.currentWeapon = weapon;
-	}		
-	
 	public void attack(Zombie zombie, int numberOfTimes) {
 		for (int i = 0; i < numberOfTimes; i++) {
-			boolean strike = currentWeapon.action();
+			boolean strike = getCurrentWeapon().action();
 			if (strike) {
 				zombie.shotsTaken++;
 			} else { // Out of bullets!
 				System.out.println("You are out of bullets, do you you wish to change weapons?");
-				//Scanner sc = System.in... //TODO implement user input
+				// Scanner sc = System.in... //TODO implement user input
 			}
 		}
 	}
-	
+
 	@Override
 	public String toString() {
-		return "**********************\n" +
-				"Number of Guns: " + this.weapons.size() + "\n" +
-				"Holding: " + this.currentWeapon.getType() +
-				"\n**********************\n";
+		StringBuilder allWeapons = new StringBuilder();
+		
+		for(Weapon weapon: this.weapons) {
+			allWeapons.append("\n\t" + weapon); 
+		}
+		return "***************" + this.name + "*************\n" + 
+				"Number of weapons: " + this.weapons.size() + "\n" +
+				"Currently holding: "	+ this.getCurrentWeapon().getType() + "\n" +
+				"Weapons available: " + allWeapons +				
+				"\n****************************\n";
 	}
+ 
 }
