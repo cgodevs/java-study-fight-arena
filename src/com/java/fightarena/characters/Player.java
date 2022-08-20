29433bb055c9643bfab6287a1b9f22b0e83184a5
cgodevs <kharol.go@gmail.com>
2022-08-19 21:56:31 -0300

    Write stats to file (use FileWriter)

--- src/com/java/fightarena/characters/Player.java
+++ src/com/java/fightarena/characters/Player.java
@@ -1,19 +1,59 @@
 package com.java.fightarena.characters;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 import com.java.fightarena.abstractions.*;
 import com.java.fightarena.exceptions.FullSlotsException;
+import com.java.fightarena.weapons.Pistol;
 
-public class Human { 
+public class Player { 
 	private String name;
+	private int level;
 	private Weapon currentWeapon; // Either a Gun or Melee Weapon
 	private ArrayList<Weapon> weapons = new ArrayList<Weapon>(4); // Only 4 weapons may be carried at a time
 	
-	public Human(String name){
-		this.name = name;
+	public Player() throws IOException { // tries to load or create player from console
+		InputStream in = null;
+		BufferedReader reader = null;
+		try {
+			in = new FileInputStream("gamestats.txt"); //format: <Name> <Level> <Current Weapon> <List Of Weapons>
+			reader = new BufferedReader(new InputStreamReader(in));			
+			String[] userInfoArray = reader.readLine().split("\\s+"); 
+			this.name = userInfoArray[0];
+			this.level = Integer.parseInt(userInfoArray[1]);
+		} catch(FileNotFoundException fnfex) {
+			in = System.in;
+			reader = new BufferedReader(new InputStreamReader(in));
+			this.name = reader.readLine();
+			this.level = 1;
+			writeStatsToFile();
+		} finally {
+			this.pickUpWeapon(new Pistol()); //TODO move to catch block when weapons are loaded from stats
+			in.close();
+			reader.close();
+		}
 	}
 	
+	private void writeStatsToFile() {
+		try {
+			FileWriter fw = new FileWriter("gamestats.txt");
+			BufferedWriter bw = new BufferedWriter(fw);			
+			bw.write(this.name + " " + this.level);  //TODO write other stats
+			bw.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
 	public Weapon getCurrentWeapon() {
 		return this.currentWeapon;
 	}
@@ -21,7 +61,7 @@ public class Human {
 	public void setCurrentWeapon(Weapon weapon) { 
 		this.currentWeapon = weapon; 
 	}
-
+	
 	// POLIMORFISM taken
 	public void pickUpWeapon(Weapon weapon) throws FullSlotsException {
 		int numberOfWeapons = this.weapons.size(); 
@@ -71,6 +111,7 @@ public class Human {
 		this.weapons.forEach((w) -> allWeapons.append("\n\t" + w)); 
 		
 		return "***************" + this.name + "*************\n" + 
+				"Current Level: " + this.level + "\n" +
 				"Number of weapons: " + this.weapons.size() + "\n" +
 				"Currently holding: "	+ this.getCurrentWeapon().getType() + "\n" +
 				"Weapons available: " + allWeapons +				
