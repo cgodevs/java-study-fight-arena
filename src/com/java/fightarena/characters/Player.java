fe6890ffd3624f2c916be77a509cf2270fa4eb30
cgodevs <kharol.go@gmail.com>
2022-08-22 23:05:49 -0300

    Add constructor and modify Exception throws

--- src/com/java/fightarena/characters/Player.java
+++ src/com/java/fightarena/characters/Player.java
@@ -17,80 +17,69 @@ public class Player {
 	private Weapon currentWeapon; // Either a Gun or Melee Weapon
 	private ArrayList<Weapon> weapons = new ArrayList<>(4); // Only 4 weapons may be carried at a time
 
-	public Player() throws IOException { // tries to load or create player from console
-		try {
-			Scanner scanner = new Scanner(new File("gamestats.txt")); //format: <Name> <Level> <Current Weapon> <List Of Weapons>
+	public Player() throws FileNotFoundException{ // tries to load or create player from console
+		Scanner scanner = new Scanner(new File("gamestats.txt")); //format: <Name> <Level> <Current Weapon> <List Of Weapons>
 
-			//GET PLAYER INFO
-			Scanner playerStats = new Scanner(scanner.nextLine());
-			playerStats.useDelimiter(",");
-			this.name = playerStats.next();
-			this.level = playerStats.nextInt();
-			playerStats.close();
+		//GET PLAYER INFO
+		Scanner playerStats = new Scanner(scanner.nextLine());
+		playerStats.useDelimiter(",");
+		this.name = playerStats.next();
+		this.level = playerStats.nextInt();
+		playerStats.close();
+		
+		//GET CURRENT WEAPON INFO
+		while(scanner.hasNext()) {
+			Scanner oneWeaponStats = new Scanner(scanner.nextLine());
+			oneWeaponStats.useLocale(Locale.US); // JVM is forced to use dots as decimals separators 
+			oneWeaponStats.useDelimiter(",");
+			String weaponType = oneWeaponStats.next();
+			String weaponName = oneWeaponStats.next();
+			double durability = oneWeaponStats.nextDouble();
+			Weapon w = null;
 			
-			//GET CURRENT WEAPON INFO
-			while(scanner.hasNext()) {
-				Scanner oneWeaponStats = new Scanner(scanner.nextLine());
-				oneWeaponStats.useLocale(Locale.US); // JVM is forced to use dots as decimals separators 
-				oneWeaponStats.useDelimiter(",");
-				String weaponType = oneWeaponStats.next();
-				String weaponName = oneWeaponStats.next();
-				double durability = oneWeaponStats.nextDouble();
-				Weapon w = null;
-				
-				if (weaponType.compareTo("MeleeWeapon") != 0) {
-					int bullets = oneWeaponStats.nextInt();
-					int cartridges = oneWeaponStats.nextInt();
-					switch(weaponName) {
-						case("Pistol"):
-							w = new Pistol(bullets, cartridges);
-							break;
-						case("Marksman Rifle"):
-							w = new MarksmanRifle(bullets, cartridges);
-							break;
-						case("Shotgun"):
-							w = new Shotgun(bullets, cartridges);
-							break;
-						case("Tactical Rifle"):
-							w = new TacticalRifle(bullets, cartridges);
-							break;
-					}
-				} else { //Load Melee Weapon
-					switch(weaponName) {
-						case("Axe"):
-							w = new Axe();
-							break;
-					}
+			if (weaponType.compareTo("MeleeWeapon") != 0) {
+				int bullets = oneWeaponStats.nextInt();
+				int cartridges = oneWeaponStats.nextInt();
+				switch(weaponName) {
+					case("Pistol"):
+						w = new Pistol(bullets, cartridges);
+						break;
+					case("Marksman Rifle"):
+						w = new MarksmanRifle(bullets, cartridges);
+						break;
+					case("Shotgun"):
+						w = new Shotgun(bullets, cartridges);
+						break;
+					case("Tactical Rifle"):
+						w = new TacticalRifle(bullets, cartridges);
+						break;
 				}
-				w.setDurability(durability);
-				this.weapons.add(w);
-				oneWeaponStats.close();
+			} else { //Load Melee Weapon
+				switch(weaponName) {
+					case("Axe"):
+						w = new Axe();
+						break;
+				}
 			}
-			setCurrentWeapon(weapons.get(0));						
-			scanner.close();
-
-		} catch(FileNotFoundException fnfex) {
-			Scanner scanner = new Scanner(System.in);
-			this.name = scanner.nextLine();
-			this.level = 1;
-			scanner.close();
-			pickUpWeapon(new Pistol()); // Default weapon for game start
-			saveGame(); 
+			w.setDurability(durability);
+			this.weapons.add(w);
+			oneWeaponStats.close();
 		}
+		setCurrentWeapon(weapons.get(0));						
+		scanner.close();
 	}
-
-	public void saveGame() throws IOException{
-		PrintWriter pw = new PrintWriter("gamestats.txt");
-		pw.write(this.name + "," + this.level);
-		pw.println();
-		for(Weapon w: this.weapons) {
-			pw.write(w.getClass().getSuperclass().getSimpleName() + "," + w.getType() + "," + w.getDurability());
-			try {
-				pw.write("," + ((Gun)w).getBulletsAvailable() + "," + ((Gun)w).getCartridgesAvailable());
-			} catch (ClassCastException e) { }
-			pw.println();				
+	
+	public Player(int i) {
+		Scanner scanner = new Scanner(System.in);
+		this.name = scanner.nextLine();
+		this.level = 1;
+		scanner.close();
+		pickUpWeapon(new Pistol()); // Default weapon for game start
+		try {
+			saveGame();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		pw.close();
 	}
 
 	public Weapon getCurrentWeapon() {
@@ -122,7 +111,7 @@ public class Player {
 					}
 				}
 			} catch (ClassCastException ex) { // Tried to reload Melee weapon before
-				((MeleeWeapon) this.getCurrentWeapon()).restore();
+				((MeleeWeapon) weapon).restore();
 			}
 		} else if (numberOfWeapons == 4) {
 			throw new FullSlotsException("You can't pick up a new weapon. Empty 1 or more slots.");
@@ -142,6 +131,20 @@ public class Player {
 				// Scanner sc = System.in... //TODO implement user input
 			}
 		}
+	}
+
+	public void saveGame() throws IOException{
+		PrintWriter pw = new PrintWriter("gamestats.txt");
+		pw.write(this.name + "," + this.level);
+		pw.println();
+		for(Weapon w: this.weapons) {
+			pw.write(w.getClass().getSuperclass().getSimpleName() + "," + w.getType() + "," + w.getDurability());
+			try {
+				pw.write("," + ((Gun)w).getBulletsAvailable() + "," + ((Gun)w).getCartridgesAvailable());
+			} catch (ClassCastException e) { }
+			pw.println();				
+		}
+		pw.close();
 	}
 
 	@Override
